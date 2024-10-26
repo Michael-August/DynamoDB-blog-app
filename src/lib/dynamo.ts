81765f01e46e0a8699bbb3423140d4dd844b1db8
Michael-August <michaelfrancis135@gmail.com>
2024-10-26 11:03:02 +0100

    Credentials check done

--- src/lib/dynamo.ts
+++ src/lib/dynamo.ts
@@ -1,14 +1,21 @@
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
+const region = process.env.AWS_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+
+if (!region || !accessKeyId || !secretAccessKey) {
+    throw new Error("AWS configuration is missing environment variables");
+}
+
 const client = new DynamoDBClient({
-    region: process.env.AWS_REGION,
+    region,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId,
+        secretAccessKey,
     },
 });
-
 const dynamoDb = DynamoDBDocumentClient.from(client);
 
 export { dynamoDb };
