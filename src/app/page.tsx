1e95a882111158be5b97ffe363a74bd626779804
Michael-August <michaelfrancis135@gmail.com>
2024-10-28 10:55:24 +0100

    mobile nav bar in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,9 +36,7 @@ export default function Home() {
         <div className="details text-center md:text-left leading-6 text-sm lg:text-base lg:leading-10">
           <span className="font-semibold text-gray-800 text-2xl lg:text-3xl">Ewere Diagboya </span>
           <span>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima ut excepturi ex tenetur sequi, atque natus mollitia, repellendus, voluptatibus cum quod maxime iure consequuntur ipsum. Cupiditate earum dolore non ullam. Dolorum, laudantium quae sequi commodi aliquam nihil reiciendis quis doloribus eaque aperiam optio magni dolor nostrum esse ea atque fugiat.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima ut excepturi ex tenetur sequi, atque natus mollitia, repellendus, voluptatibus cum quod maxime iure consequuntur ipsum. Cupiditate earum dolore non ullam. Dolorum, laudantium quae sequi commodi aliquam nihil reiciendis quis doloribus eaque aperiam optio magni dolor nostrum esse ea atque fugiat.
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima ut excepturi ex tenetur sequi, atque natus mollitia, repellendus, voluptatibus cum quod maxime iure consequuntur ipsum. Cupiditate earum dolore non ullam. Dolorum, laudantium quae sequi commodi aliquam nihil reiciendis quis doloribus eaque aperiam optio magni dolor nostrum esse ea atque fugiat.
+            started his career in tech in 2003. He started as a Software Developer in PHP, Visual Basic, HTML, and CSS. He later switched to DevOps and Cloud in 2015. He is the first AWS Community Hero in Africa and the author of two books: Infrastructure Monitoring with Amazon CloudWatch and Techtionary which are available on Amazon.com. He is an AWS Community leader in Nigeria. He loves to talk about Cloud Computing, DevOps, and innovations around efficient software delivery technologies and processes. He has also been a two-time judge for the Cybersafe Foundation Cybergirls Fellowship and PipeOps Hackathon.
           </span>
         </div>
       </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,30 +1,41 @@
 import Link from 'next/link'
 import React from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
+import { FaHamburger } from 'react-icons/fa'
+import { PiHamburger } from 'react-icons/pi'
 
 const NavBar = () => {
     return (
-        <nav className='fixed bg-[#f7fbff] z-50 top-0 flex items-center w-full justify-between mx-auto px-2 md:px-4 container py-4'>
-            <div className="logo text-xl font-semibold">
-                <Link href={"/"}>Ewere Diagboya</Link>
-            </div>
-            <ul className='flex items-center gap-5'>
-                <li>
-                    <Link href={"/"}>Home</Link>
-                </li>
-                <li>
-                    <Link href={"/blog"}>Blog</Link>
-                </li>
-                <li>
-                    <Link href={"/blog"}>Contact</Link>
-                </li>
-                {/* <li>
-                    <Link href={"/blog"}>Blog</Link>
-                </li> */}
-            </ul>
-            <div className='border-2 border-black px-4 py-2 hover:bg-black hover:text-white rounded-tl-2xl rounded-br-2xl cursor-pointer'>
-                Eweres Blog
-            </div>
-        </nav>
+        <div className='relative '>
+            <nav className='fixed bg-[#f7fbff] z-50 top-0 flex items-center w-full justify-between mx-auto px-2 md:px-4 container py-4'>
+                <div className="logo text-base lg:text-xl font-semibold">
+                    <Link href={"/"}>Ewere Diagboya</Link>
+                </div>
+                <ul className='hidden lg:flex lg:items-center lg:gap-5'>
+                    <li>
+                        <Link href={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link href={"/blog"}>Blog</Link>
+                    </li>
+                    <li>
+                        <Link href={"/blog"}>Contact</Link>
+                    </li>
+                    {/* <li>
+                        <Link href={"/blog"}>Blog</Link>
+                    </li> */}
+                </ul>
+                <div className='hidden lg:block border-2 border-black px-4 py-2 hover:bg-black hover:text-white rounded-tl-2xl rounded-br-2xl cursor-pointer'>
+                    Eweres Blog
+                </div>
+                <div className="icon block lg:hidden cursor-pointer">
+                    <AiOutlineMenu />
+                </div>
+            </nav>
+            {/* <div className={`for-mobile lg:hidden absolute top-0 left-0 w-screen h-screen bg-white`}>
+
+            </div> */}
+        </div>
     )
 }
 
