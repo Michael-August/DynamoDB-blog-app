4435a869898766d63ab428c0871140df22c3884a
Michael-August <michaelfrancis135@gmail.com>
2024-11-22 10:28:09 +0100

    ANALYTICS FIX

--- src/app/layouts/root.tsx
+++ src/app/layouts/root.tsx
@@ -29,6 +29,26 @@ export default function MainRootLayout({ children,
           strategy="afterInteractive"
         />
 
+        {/* Google Analytics Scripts */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+
       </head>
       <body className="container mx-auto bg-[#f7fbff]">
         <GoogleAnalytics gaMeasurementId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} trackPageViews />
